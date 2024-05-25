4dcfe8ab376992f45c78c31762d850bb18d99b60
samL23 <samuellavictoire1@gmail.com>
2024-05-24 22:32:52 -0400

    fixed flag logic for cli add option

--- cmd/cliTodoApp/main.go
+++ cmd/cliTodoApp/main.go
@@ -2,23 +2,36 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"os"
 
 	jsonhandling "github.com/samL23/cliTodoApp/internal/jsonHandling"
 )
 
 func main() {
+	const fileName = "data.json"
 	// fetch the previous todo elements from json files
 	// add todo element into a slice
-	fileName := "data.json"
+	var title *string
+	var desc *string
+
+	if os.Args[1] == "add" {
+		//if flag has add option make a new flag set that looks at the args after flag add
+		addCmd := flag.NewFlagSet("add", flag.ExitOnError)
+		title = addCmd.String("title", "root", "a string")
+		desc = addCmd.String("d", "", "description")
+		addCmd.Parse(os.Args[2:])
+	}
+
+	//parse original full flag
 	flag.Parse()
-	fmt.Println(flag.Arg(0))
 
 	switch flag.Arg(0) {
 	case "":
 		//error print help
 	case "list":
 		jsonhandling.ListAll(fileName)
+	case "add":
+		jsonhandling.AddItem(*title, *desc, fileName)
 	}
 
 }
--- internal/jsonHandling/jsonHandling.go
+++ internal/jsonHandling/jsonHandling.go
@@ -14,9 +14,20 @@ type Todo struct {
 	Id        int
 }
 
-func SaveToJson(list []Todo) {
-	//save each todo item in json file
+func ListAll(fileName string) {
+	todos := readJSON(fileName)
+	printOut(todos)
 
+}
+
+//-----
+
+func saveToJson(list []Todo, filename string) {
+	//save each todo item in json file
+	file, _ := os.OpenFile(filename, os.O_CREATE, os.ModePerm)
+	defer file.Close()
+	encoder := json.NewEncoder(file)
+	encoder.Encode(list[0])
 }
 
 func readJSON(fileName string) []Todo {
@@ -37,14 +48,28 @@ func readJSON(fileName string) []Todo {
 	return todo
 }
 
+func AddItem(title string, desc string, filename string) {
+	//get list of exsisting items from json
+	fmt.Println("adding..")
+	todos := readJSON(filename)
+
+	//add the item
+	if title == "" {
+		fmt.Println("error: item title empty :" + title)
+		return
+	}
+
+	id := todos[len(todos)-1].Id + 1
+	item := Todo{title, desc, false, id}
+	todos = append(todos, item)
+	fmt.Println(todos)
+	//save json file (overwrite with new list)
+
+	saveToJson(todos, filename)
+}
+
 func printOut(todoSlice []Todo) {
 	for _, s := range todoSlice {
 		fmt.Printf(" Id: %d - Title: %s - Desc: %s - Completed: %t \n", s.Id, s.Title, s.Desc, s.Completed)
 	}
-}
-
-func ListAll(fileName string) {
-	todos := readJSON(fileName)
-	printOut(todos)
-
 }
